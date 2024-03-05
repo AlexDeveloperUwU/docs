d9802df167c52de92e8e52e33c5ca393a9ca124b
AlexDeveloperUwU <79850072+AlexDeveloperUwU@users.noreply.github.com>
2024-03-05 14:09:29 +0100

    Update config.mjs

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -3,7 +3,15 @@ import { defineConfig } from "vitepress";
 export default defineConfig({
   title: "Documentación",
   description: "La documentación oficial para todos los proyectos de AlexDeveloperUwU",
+  ignoreDeadLinks: true,
   themeConfig: {
+    search: {
+      provider: 'local'
+    },
+    footer: {
+      message: 'Esta documentación ha sido creada por @AlexDeveloperUwU',
+      copyright: 'Documentación bajo la licencia GPL-3.0'
+    },    
     aside: false,
     nav: [
       {
